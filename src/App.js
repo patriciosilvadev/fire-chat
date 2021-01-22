1885783d02a0ef4b341a31e5471095124dfe7119
turbolukoshko <turbo.lukoshko@gmail.com>
2021-01-23 00:59:41 +0200

    added a state of auth in app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { PublicRoute } from './HOC/PublicRoute';
@@ -8,16 +8,28 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 import Chat from './components/Chat/Chat';
-import { useState } from 'react';
+import { auth } from './services/firebase';
 
 function App() {
 
+  useEffect(() => {
+    auth().onAuthStateChanged((user) => {
+      if(user) {
+        setAuthenticated(true);
+        setLoading(false);
+      } else {
+        setAuthenticated(false);
+        setLoading(false);
+      }
+    });
+  }, []);
+
   const [authnticated, setAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   return (
     loading ?
-    <h1>Loading</h1> :
+    <h1>Loading ...</h1> :
     (<BrowserRouter>
       <Switch>
         <PublicRoute path={LOGIN_ROUTE} component={Login} authnticated={authnticated} />
